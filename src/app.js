35b9a24767f2c600a2276ca09da920db8dabcfc2
EstebanAcosta117 <estebanacostas117@gmail.com>
2024-09-25 16:35:25 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ const path = require ('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const indexRoutes = require ('./routes/index.routes');
+const productsRoutes = require ('./routes/products.routes')
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin.html')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.use('/', indexRoutes);
+app.use('/products', productsRoutes);
+
+
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/product-details', (req, res) => res.sendFile(path.join(__dirname, 'views', 'product-details.html')));
-app.get('/products', (req, res) => res.sendFile(path.join(__dirname, 'views', 'products.html')));
 app.get('/profile', (req, res) => res.sendFile(path.join(__dirname, 'views', 'profile.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
-
 
 app.listen(PORT, () => console.log('servidor corriendo en http://localhost:' + PORT))
---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+const path = require ('path');
+
+module.exports = {  
+    register : (req,res) => {
+
+    },
+    login : (req,res) => {
+
+    },
+    profile : (req,res) => {
+        
+    }
+} 
